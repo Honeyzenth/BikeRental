a02b7fe880efcefb5f1165256e360f4774433329
Honeyzenth Flores <honeyzenth@gmail.com>
2025-03-09 13:12:54 +0800

    Added complexity

--- BikeRental/Signup.cs
+++ BikeRental/Signup.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -23,50 +24,72 @@ namespace BikeRental
         public static string phonenumber = "";
         public static string address = "";
 
+        private bool IsPasswordComplex(string password)
+        {
+            // Password must be at least 8 characters long, contain an uppercase letter, 
+            // a lowercase letter, a number, and a special character.
+            string pattern = @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$";
+
+            return Regex.IsMatch(password, pattern);
+        }
+
         private void btnCreateAccount_Click(object sender, EventArgs e)
         {
             try
             {
                 if (txtPassword.Text != txtConfirmPassword.Text)
                 {
-                    MessageBox.Show("Mismatched password ", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    MessageBox.Show("Mismatched password", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
                     CLEAR();
+                    return;
                 }
-                else if (txtFullname.Text == "" || txtEmail.Text == "" || txtPhoneNumber.Text == "" || txtAdd.Text == "" || txtUserName.Text == "" || txtPassword.Text == "" || txtConfirmPassword.Text == "")
+
+                if (txtFullname.Text == "" || txtEmail.Text == "" || txtPhoneNumber.Text == "" ||
+                    txtAdd.Text == "" || txtUserName.Text == "" || txtPassword.Text == "" || txtConfirmPassword.Text == "")
                 {
-                    MessageBox.Show("fill up required field ", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    MessageBox.Show("Fill up required fields", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
                     CLEAR();
+                    return;
                 }
-                else
+
+                // Check password complexity
+                if (!IsPasswordComplex(txtPassword.Text))
                 {
-                    Encryption encrypt = new Encryption();
-                    string password = txtPassword.Text;
-                    string hashedPassword = encrypt.HashPassword(password);
-
-                    sql = "INSERT INTO [Customer_Signup] ([username], [password], [Name], [Email], [PhoneNumber],[Address]  )" +
-                        "VALUES('" + txtUserName.Text + "'," +
-                       "'" + hashedPassword + "', " +
-                       "'" + txtFullname.Text + "', " +
-                        "'" + txtEmail.Text + "', " +
-                        "'" + txtPhoneNumber.Text + "'," +
-                        "'" + txtAdd.Text + "')";
-                    DBhelper.DBhelper.ModifyRecord(sql);
-                    MessageBox.Show("Account Created!");
-
-                    fullname = txtFullname.Text;
-                    email = txtEmail.Text;
-                    phonenumber = txtPhoneNumber.Text;
-                    address = txtAdd.Text;
-                    loginForm L = new loginForm();
-                    L.Show();
-                    this.Hide();
+                    MessageBox.Show("Password must be at least 8 characters long and include a number, an uppercase letter, a lowercase letter, and a special character.",
+                        "Weak Password", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
                 }
+
+                // Hash and salt password
+                Encryption encrypt = new Encryption();
+                string password = txtPassword.Text;
+                string hashedPassword = encrypt.HashPassword(password);
+
+                sql = "INSERT INTO [Customer_Signup] ([username], [password], [Name], [Email], [PhoneNumber], [Address]) " +
+                      "VALUES('" + txtUserName.Text + "'," +
+                      "'" + hashedPassword + "', " +
+                      "'" + txtFullname.Text + "', " +
+                      "'" + txtEmail.Text + "', " +
+                      "'" + txtPhoneNumber.Text + "'," +
+                      "'" + txtAdd.Text + "')";
+
+                DBhelper.DBhelper.ModifyRecord(sql);
+                MessageBox.Show("Account Created!");
+
+                fullname = txtFullname.Text;
+                email = txtEmail.Text;
+                phonenumber = txtPhoneNumber.Text;
+                address = txtAdd.Text;
+                loginForm L = new loginForm();
+                L.Show();
+                this.Hide();
             }
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message);
             }
         }
+
         public void CLEAR()
         {
             txtConfirmPassword.Clear();
